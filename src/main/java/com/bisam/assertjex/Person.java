df1ed37e6f030197d04c4e6d8252075d43523ae1
unknown <RD@PC4-DEV.bi-sam.com>
2014-06-23 15:34:58 +0200

    Strong typing test

--- src/main/java/com/bisam/assertjex/Person.java
+++ src/main/java/com/bisam/assertjex/Person.java
@@ -3,10 +3,12 @@ package com.bisam.assertjex;
 public class Person {
   private final String name;
   private final String surname;
+  private final int age;
 
-  public Person(String surname, String name) {
+  public Person(String surname, String name, int age) {
     this.name = name;
     this.surname = surname;
+    this.age = age;
   }
 
   public String getName() {
@@ -15,5 +17,9 @@ public class Person {
 
   public String getSurname() {
     return surname;
+  }
+
+  public int getAge() {
+    return age;
   }
 }
--- src/test/java/com/bisam/assertjex/ListsTest.java
+++ src/test/java/com/bisam/assertjex/ListsTest.java
@@ -4,25 +4,52 @@ import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.extractProperty;
+import static org.assertj.core.api.Assertions.tuple;
+
 public class ListsTest {
   private Persons yellowTeam;
 
   @Before
   public void setUp() {
     yellowTeam = new Persons();
-    yellowTeam.add(new Person("Pascal", "Ekouaghe"))
-      .add(new Person("Guillaume", "Michel"))
-      .add(new Person("Fouad", "Hellou"))
-      .add(new Person("Fatima", "Amach"));
+    yellowTeam.add(new Person("Pascal", "Ekouaghe", 27))
+      .add(new Person("Guillaume", "Michel", 35))
+      .add(new Person("Fouad", "Hellou", 29))
+      .add(new Person("Fatima", "Amach", 31));
   }
 
   @Test
   public void testSurnames() throws Exception {
-    Assertions.assertThat(yellowTeam).extracting("surname").containsOnly("Pascal", "Guillaume", "Fouad", "Fatima");
+    Assertions.assertThat(yellowTeam).extracting("surname").containsOnly("Pascal", "Fatima", "Guillaume", "Fouad");
   }
 
   @Test
   public void testSurnamesInError() throws Exception {
     Assertions.assertThat(yellowTeam).extracting("surname").containsOnly("Pascal", "Guillaume", "Fouad");
+  }
+
+  @Test
+  public void testNamesAndSurnames() throws Exception {
+    Assertions.assertThat(yellowTeam).extracting("surname", "name")
+      .containsOnly(tuple("Pascal", "Ekouaghe"),
+                    tuple("Guillaume", "Michel"),
+                    tuple("Fouad", "Hellou"),
+                    tuple("Fatima", "Amach"));
+
+  }
+
+  @Test
+  public void testNamesAndSurnamesInError() throws Exception {
+    Assertions.assertThat(yellowTeam).extracting("surname", "name")
+      .containsOnly(tuple("Guillaume", "Michel"),
+                    tuple("Fouad", "Hellou"),
+                    tuple("Fatima", "Amach"));
+
+  }
+
+  @Test
+  public void testAgeWithStrongTypingExtraction() throws Exception {
+    Assertions.assertThat(extractProperty("age", Integer.class).from(yellowTeam)).containsOnly(31, 35, 29, 27);
   }
 }
